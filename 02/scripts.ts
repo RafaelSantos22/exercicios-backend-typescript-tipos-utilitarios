b80e70b032451dc2dd6d70f3ce4773e1f0c6d414
Rafael Santos <rafa_santos33@outlook.com>
2024-09-09 17:38:17 -0300

    Exercício finalizado

--- 02/scripts.ts
+++ 02/scripts.ts
@@ -19,3 +19,25 @@ type Carrinho = {
     tipoTransacao: 'credito' | 'Debito',
     cartao: Cartao
 }
+
+type Endereco = {
+    cep: string,
+    rua: string,
+    bairro: string,
+    cidade: string,
+    estado: string
+}
+
+type NovoCarrinho = Carrinho & {
+    endereco: Endereco  
+}
+
+// type NovoCarrinho = {
+//     item: Item,
+//     qtd: number,
+//     desconto: number,
+//     frete: number,
+//     tipoTransacao: Lowercase<'credito' | 'Debito'>
+//     cartao: Cartao,
+//     endereco: Endereco
+// }
